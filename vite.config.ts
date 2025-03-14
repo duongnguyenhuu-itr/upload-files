a643b5e5e863b7c8aaf8d8a4ac436d55ac6b4ad9
DuongNguyen <duongnguyenhuu@itrvn.com>
2025-03-14 13:58:00 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,17 @@ export default defineConfig({
   server: {
     port: 3001,
   },
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
   define: {
     'process.platform': JSON.stringify(process.platform),
